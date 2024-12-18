dd1babdf49723381de7c2e8788e72211c1b80f3b
Anonnwesha Guha <169167824+anonnwesha@users.noreply.github.com>
2024-12-19 01:48:49 +0530

    Delete employeemanagementsystem directory

--- employeemanagementsystem/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO departments (name) VALUES ('HR');
-INSERT INTO departments (name) VALUES ('IT');
-
-INSERT INTO employees (name, email, department_id) VALUES ('Meenakshi Sharma', 'meenasharma@gmail.com', 1);
-INSERT INTO employees (name, email, department_id) VALUES ('Komal Sharma', 'kskomal23@gmail.com', 2);
